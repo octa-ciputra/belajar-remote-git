c0126c0a85bee1295d619ae3139864e2cd9a3c7d
octa-ciputra <octavian@ciputra.ac.id>
2024-11-08 13:08:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from modules.jumlah import tambah
 
 
 A = 5
-B = 4 
-print(f'hasil penjumlahan {A} dan {B} adalah {tambah(A,B)}')
+B = 4
+C = tambah(A+B)
+print(f'hasil penjumlahan {A} dan {B} adalah {C}')
